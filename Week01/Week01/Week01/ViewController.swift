b6b8c5257123da1616523a3b2f36d422bbde7b02
Brett Rapp <brettrapp@mac.com>
2020-05-30 12:44:31 +1000

    Addedd oulets and actions

--- Week01/Week01/Week01/ViewController.swift
+++ Week01/Week01/Week01/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var redValue = 0
+    var greenValue = 0
+    var blueValue = 0
 
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func setColor(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "String", message: "This is the message string", preferredStyle:   .alert)
+        let action = UIAlertAction.init(title: "ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func redSliderMoved(_ sender: Any) {
+        
+    }
+    
+    @IBAction func greenSliderMoved(_ sender: Any) {
+        
+    }
+    
+    @IBAction func blueSliderMoved(_ sender: Any) {
+        
+    }
+    
 
+    @IBAction func resetButton(_ sender: Any) {
+        
+    }
+    
 }
 
